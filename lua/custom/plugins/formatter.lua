8982100201a4a94e3546a1043f8fac911acf5b77
SamPosh <samselvaprabu@gmail.com>
2023-05-21 20:14:55 +0530

    Update formatter.lua
    null-ls formatting

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,14 +1,27 @@
 return {
     "jose-elias-alvarez/null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
+    dependencies = {
+        'williamboman/mason.nvim',
+    },
     opts = function()
       local nls = require("null-ls")
       return {
         sources = {
           nls.builtins.formatting.black.with({
              extra_args = {"--line-length=120", "--skip-string-normalization"},
+             filetypes = {
+                "python"
+              },
           }),
+          nls.builtins.formatting.prettier.with({
+              extra_args = {
+                "--print-with", "80"
+              },
+              filetypes = {
+                "typescript", "typescriptreact", "javascript", "vue"
+              },
+            }),
         },
       }
     end,
