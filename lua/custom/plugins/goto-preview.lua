27275369ab8c8b052c3a2b08f7410702ccf7dca9
SamPosh <samselvaprabu@gmail.com>
2023-05-22 10:39:53 +0530

    Update goto-preview.lua
    Keymap set

--- lua/custom/plugins/goto-preview.lua
+++ lua/custom/plugins/goto-preview.lua
@@ -2,5 +2,6 @@ return {
   'rmagatti/goto-preview',
   config = function()
     require('goto-preview').setup {}
-  end
+  end,
+  vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", {noremap=true})
 }
